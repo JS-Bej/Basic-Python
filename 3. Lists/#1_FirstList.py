9d764a8ad60c5f4496484e0946d16ada3a694f42
JB <sebastian03741@gmail.com>
2025-05-06 20:49:50 -0500

    6/05.1

---
+++ 3. Lists/#1_FirstList.py
@@ -0,0 +1,16 @@
+
+# In python, a list is a collection of items that are ordered and changeable.
+# Lists are written with square brackets `[]`, and the items in the list are separated by commas:
+
+first_list = [1, 2, 3, 4, 5]
+
+# Lists can contain any type of data, including numbers, strings, and even other lists.
+# Lists are also mutable, meaning you can change their content without changing their identity:
+
+first_list = [True, 2, 3.0, "Four", [5, 6, 7]]
+
+# And we can print it simply by using the print() function:
+print(first_list) # Output: [True, 2, 3.0, 'Four', [5, 6, 7]]
+
+# We can also create an empty list and add items to it later:
+empty_list = []
